7a2d12e8c81236fda32ebceeb30e73098ebed32a
tongxiny <111823275+tongxiny@users.noreply.github.com>
2022-11-30 10:47:15 -0800

    Update heatmap.py
    1. data_for_tableau.csv is saved by filename now. No longer saved to a specific local path.
    2. Empty pandas.dataframe fixed. Due to daily updates in the dataset, you now get yesterday's data instead of today's if today's is not available by the time.

--- heatmap.py
+++ heatmap.py
@@ -1,19 +1,23 @@
 import pandas as pd
 from datetime import date
+from datetime import timedelta
 pd.set_option('display.max_columns', None)
 pd.set_option('display.max_rows', None)
 pd.set_option('mode.use_inf_as_na', True)
 
 def get_data(url:str):
     today = date.today().strftime("%Y-%m-%d")
+    yesterday = (date.today() - timedelta(days=1)).strftime("%Y-%m-%d")
     df = pd.read_csv(url, engine='python')
     df = df[['location', 'date', 'total_cases', 'total_deaths']]
     df.dropna(inplace=True)
-    df = df.loc[df['date'] == today, :]
+    if df.loc[df['date'] == today, :].empty:
+        df = df.loc[df['date'] == yesterday, :]
+    else:
+        df = df.loc[df['date'] == today, :]
     df = df.drop_duplicates(subset='location', keep="first")
     df = df.convert_dtypes()
-    print(df)
-    df.to_csv(r'/Users/mac/Desktop/dsci510/final/data_for_tableau.csv', index=False)
+    df.to_csv('data_for_tableau.csv', index=False)
 
 
 if __name__ == '__main__':
